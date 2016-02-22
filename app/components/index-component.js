c6b4d12d2c4fcfcb9beb85a3b7d59f1d4db1c5cf
fademovic <fadil.ademovic@gmail.com>
2016-02-22 01:19:14 +0100

    jumbotron animation

--- app/components/index-component.js
+++ app/components/index-component.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
-var value=0;
+var value=150;
+var tmp=0;
+var tmp1=0;
 
 
 export default Ember.Component.extend({  
@@ -33,17 +35,29 @@ export default Ember.Component.extend({
 
         leftSlide: function() 
         {
+            if(value==900)
+              value=750;
+
+            this.$( "div.horizontal-slider" ).scrollLeft( value );
+            value+=-150;
             
-            this.$( "div.horizontalSlider" ).scrollLeft( value );
-            value+=-100;
+            if(value==-150)
+            value=0;
 
         },
 
         rightSlide: function() 
         {
+            if(value==0)
+              value=150;
             
-            this.$( "div.horizontalSlider" ).scrollLeft( value );
-            value+=100;
+            this.$( "div.horizontal-slider" ).scrollLeft( value );
+            value+=150;
+
+            if(value>900)
+                value=900;
+            
+       
 
         },
 
